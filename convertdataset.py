67c40561b0501c9b7f8e6a82a67b3f555f3cc568
Niranjan Gopalan <niranjangopalan948@gmail.com>
2024-07-31 01:02:38 +0100

    Updated codes

--- convertdataset.py
+++ convertdataset.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
+
+dc = 'Delhi_capitals_2024.html'
+kkr = 'KKR_2024.html'
+fullStats = 'players_stats_table.html'
+teamRecord = 'teamRecord.html'
+
+
 # Read the HTML file
-html_file = 'players_stats_table.html'
+html_file = kkr
 tables = pd.read_html(html_file)
 
 # Assuming there's only one table in the HTML file
@@ -9,4 +16,4 @@ table = tables[0]
 
 # Print the table
 print(table)
-# table.to_csv('output.csv', index=False)
+# table.to_csv('kkr_2024_players_records.csv', index=False)
